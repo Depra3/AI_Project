5dd3bcb370669183310af34bf0bf7cd3bafa44f4
rlawhdgur <dlsrks_25@naver.com>
2023-02-03 15:04:17 +0900

    updated

--- suggestions.py
+++ suggestions.py
@@ -10,12 +10,12 @@ cur = conn.cursor()
 
 # 테이블 생성
 def create_tb():
-    cur.execute('CREATE TABLE IF NOT EXISTS suggestion(author CHAR, email VARCHAR, title TEXT, comment MEDIUMTEXT, date TEXT, status TEXT)' )
+    cur.execute('CREATE TABLE IF NOT EXISTS suggestion(author CHAR, email VARCHAR, title TEXT, comment TEXT, date TEXT, status TEXT)' )
     conn.commit()
 
 # db 입력
-def add_data(author, pword, title, date, comment, status):
-    params = (author, pword, title, str(date), comment, status)
+def add_data(author, email, title, date, comment, status):
+    params = (author, email, title, str(date), comment, status)
     cur.execute("INSERT INTO suggestion(author, email, title, date, comment, status) VALUES (?,?,?,?,?,?)",params)
     conn.commit()
 
@@ -24,15 +24,6 @@ def sugg_list():
     cur.execute('SELECT author, title, date, comment, status FROM suggestion')
     sugg = cur.fetchall()
     return sugg
-
-# # 수정 (update)
-# def data_update(username):
-#     pass
-# # 삭제 (delete)
-# def data_delete(username):
-#     cur.execute('DELETE FROM suggestion WHERE AUTHOR =:AUTHOR' ,{'AUTHOR':author})
-#     conn.commit()
-#     # conn.close()
 
 # 검색 (작성자명)
 def get_by_author(author):
@@ -46,7 +37,7 @@ def get_by_title(title):
 	return data
 # 검색(내용)
 def get_by_comment(comment):
-	cur.execute("SELECT author, title, date, comment, status FROM suggestion WHERE comment like '%{}%'".format(comment))
+	cur.execute("SELECT author, title, date, comment, status FROM suggestion WHERE date like '%{}%'".format(comment))
 	data = cur.fetchall()
 	return data
 
@@ -58,6 +49,11 @@ def recover_status(email):
     cur.execute('UPDATE suggestion SET status = "접수" WHERE email="{}"'.format(email))
     conn.commit()
 
+def delete_post(email):
+    cur.execute('DELETE FROM suggestion WHERE email = "{}"'.format(email))
+    conn.commit()
+     # conn.close()
+
 
 # ▲ DB 관련
 # -------------------------------------------------------------------------------------------------
@@ -65,10 +61,7 @@ def recover_status(email):
 # -------------------------------------------------------------------------------------------------
 
 def run_suggestions():
-    st.subheader("""
-    건의사항💢
-    - *궁금하시거나 불편하신 점 있으시면 게시판 등록해주세요!!*
-    """)
+    st.subheader('건의사항')
 
     # 문의사항 입력
     with st.expander("문의하기"):
@@ -81,36 +74,42 @@ def run_suggestions():
             title = st.text_input("제목", max_chars = 50)
             comment = st.text_area("내용 ")
             submit = st.form_submit_button(label="작성")
-            date = time.strftime('%Y.%m.%d %H:%M')
+            date = time.strftime('%Y.%m.%d %H:%M:%S')
             status = "접수"
             if submit:
-                
-                # 내용란에 아래 글 입력, 이메일에 처리된 이메일 입력시 "처리완료" 혹은 "접수"로 변경함
-                if comment == "ok_myroomadmin":
-                    update_status(email)
-
-                elif comment == "no_myroomadmin":
-                    recover_status(email)
-
-                else :
+                # 문의사항 접수
+                if submit:
+                    if author == "" or email == "" or title == "" or comment == "":
+                        st.error('빈칸을 확인해주세요.')
+                else:
                     add_data(author, email, title, comment, date, status)
                     st.success("문의하신 내용이 접수되었습니다! 답변은 작성해주신 이메일로 발송됩니다. 감사합니다.")
-                    st.balloons()
+                # add_data(author, email, title, comment, date, status)
+                # st.success("문의하신 내용이 접수되었습니다! 답변은 이메일로 발송됩니다. 감사합니다.")
+                    st.snow()
 
-     # 검색
+    # 검색
     with st.expander("검색"):
-        cols = st.columns((1,1,1))
-        search_term = cols[0].text_input('검색')
-        search_option = cols[1].selectbox("검색옵션",("내용","작성자명","제목"))
-        if cols[2].button("검색"):
-                if search_option == "내용":
+        cols = st.columns((1,1))
+        search_term = cols[0].text_input(' ')
+        search_option = cols[1].selectbox(" ",("--검색옵션--","내용","작성자명","제목"))
+        if st.button("검색"):
+                if search_term == "":
+                    st.warning("검색어를 입력하세요")
+                elif search_option == "내용":
                     result=get_by_comment(search_term)
+                    s_result = pd.DataFrame(result, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+                    st.dataframe(s_result, use_container_width=True)
                 elif search_option =="작성자명":
                     result=get_by_author(search_term)
+                    s_result = pd.DataFrame(result, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+                    st.dataframe(s_result, use_container_width=True)
                 elif search_option =="제목":
                     result=get_by_title(search_term)
-                s_result = pd.DataFrame(result, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
-                st.dataframe(s_result, use_container_width=True)
+                    s_result = pd.DataFrame(result, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+                    st.dataframe(s_result, use_container_width=True)
+                else :
+                    st.warning("검색옵션을 입력하세요")
 
 
     # 목록
@@ -124,6 +123,184 @@ def run_suggestions():
         # st.table(df)
 
 
+    # 관리자 기능
+    admin_option = st.checkbox("관리자 메뉴")
+    if admin_option:
+        cols = st.columns((1,1))
+        command = cols[0].text_input("command")
+        email = cols[1].text_input("email")
+        if st.button ("확인"):
+            if command == "ok_myroomadmin":
+                update_status(email)
+            elif command == "no_myroomadmin":
+                recover_status(email)
+            elif command == "del_myroomadmin":
+                st.checkbox("삭제하시겠습니까? 작성하신 이메일을 다시 확인해주세요.")
+                # del_reason = st.text_input("삭제사유")
+                delete_post(email)
+            else :
+                st.warning("잘못된 명령입니다")
+
+
+# conn = sqlite3.connect('suggestion.db', check_same_thread=False)
+# cur = conn.cursor()
+
+# # conn_1 = sqlite3.connect("text.db")
+# # cur_1 = conn_1.cursor()
+# # test_t = "('CREATE TABLE IF NOT EXISTS test(author CHAR not null, email VARCHAR not null, title TEXT not null, comment MEDIUMTEXT not null, date TEXT not null, status TEXT not null)' )"
+# # cur_1.execute(test_t)
+# # conn.commit()
+
+# # 테이블 생성
+# def create_tb():
+#     # cur.execute('CREATE TABLE IF NOT EXISTS suggestion(author CHAR, email VARCHAR, title TEXT, comment MEDIUMTEXT, date TEXT, status TEXT)')
+#     cur.execute('CREATE TABLE IF NOT EXISTS suggestion1111(author CHAR, email VARCHAR, title TEXT, comment TEXT, date TEXT, status TEXT)')
+    
+#     conn.commit()
+
+# # db 입력
+# # def add_data(author, email, title, date, comment, status):
+# #     params = (author, email, title, str(date), comment, status)
+# #     cur.execute("INSERT INTO suggestion11(author, email, title, date, comment, status) VALUES (?,?,?,?,?,?)",params)
+# #     conn.commit()
+# def add_data(author, email, title, comment, date, status):
+#     params = (author, email, title, comment, str(date), status)
+#     cur.execute("INSERT INTO suggestion1111(author, email, title, comment, date, status) VALUES (?,?,?,?,?,?)",params)
+#     conn.commit()
+
+# # 목록 불러오기
+# def sugg_list():
+#     cur.execute('SELECT author, title, comment, date, status FROM suggestion1111')
+#     sugg = cur.fetchall()
+#     return sugg
+
+# # # 수정 (update)
+# # def data_update(username):
+# #     pass
+# # # 삭제 (delete)
+# # def data_delete(username):
+# #     cur.execute('DELETE FROM suggestion WHERE AUTHOR =:AUTHOR' ,{'AUTHOR':author})
+# #     conn.commit()
+# #     # conn.close()
+
+# # 검색 (작성자명)
+# def get_by_author(author):
+# 	cur.execute("SELECT author, title, coment, date, status FROM suggestion1111 WHERE author like '%{}%'".format(author))
+# 	data = cur.fetchall()
+# 	return data
+# # 검색(제목)
+# def get_by_title(title):
+# 	cur.execute("SELECT author, title, comment, date, status FROM suggestion111 WHERE title like '%{}%'".format(title))
+# 	data = cur.fetchall()
+# 	return data
+# # 검색(내용)
+# def get_by_comment(comment):
+# 	cur.execute("SELECT author, title, comment, comment, status FROM suggestion111 WHERE commet like '%{}%'".format(comment))
+# 	data = cur.fetchall()
+# 	return data
+
+# # 처리상태 수정
+# def update_status(email):
+#     cur.execute('UPDATE suggestion111 SET status = "처리완료" WHERE email="{}"'.format(email))
+#     conn.commit()
+# def recover_status(email):
+#     cur.execute('UPDATE suggestion111 SET status = "접수" WHERE email="{}"'.format(email))
+#     conn.commit()
+
+
+# # ▲ DB 관련
+# # -------------------------------------------------------------------------------------------------
+# # -------------------------------------------------------------------------------------------------
+# # -------------------------------------------------------------------------------------------------
+
+# def run_suggestions():
+#     st.subheader("""
+#     건의사항💢
+#     - *궁금하시거나 불편하신 점 있으시면 게시판 등록해주세요!!*
+#     """)
+
+#     # 문의사항 입력
+#     with st.expander("문의하기"):
+#         form = st.form(key="annotation")
+#         with form:
+#             create_tb()
+#             cols = st.columns((1,1))
+#             author = cols[0].text_input("작성자명 ", max_chars = 12)
+#             email = cols[1].text_input("이메일 ")
+#             title = st.text_input("제목", max_chars = 50)
+#             comment = st.text_area("내용 ")
+#             submit_cols = st.columns((1,1))
+#             submit = submit_cols[0].form_submit_button(label="작성")
+#             submit_1 = submit_cols[1].form_submit_button(label="수정")   
+#             date = time.strftime('%Y.%m.%d %H:%M')
+#             status = "접수"
+#             if submit:
+#                 if author == "" or email == "" or title == "" or comment == "":
+#                     st.error('빈칸을 확인하세요')
+#                 else:
+#                     add_data(author, email, title, comment, date, status)
+#                     st.success("문의하신 내용이 접수되었습니다! 답변은 작성해주신 이메일로 발송됩니다. 감사합니다.")
+#             if submit_1:
+#                 # 내용란에 아래 글 입력, 이메일에 처리된 이메일 입력시 "처리완료" 혹은 "접수"로 변경함
+#                 if comment == "ok_myroomadmin":
+#                     update_status(email)
+
+#                 elif comment == "no_myroomadmin":
+#                     recover_status(email)
+#                 else:
+#                      st.error("관리자 권한입니다!!")
+#             #     else :
+#             #         add_data(author, email, title, comment, date, status)
+#             #         st.success("문의하신 내용이 접수되었습니다! 답변은 작성해주신 이메일로 발송됩니다. 감사합니다.")
+#             #         st.balloons()
+
+#     # 검색
+#     with st.expander("검색"):
+#         cols = st.columns((1,1,1))
+#         search_term = cols[0].text_input('검색')
+#         search_option = cols[1].selectbox("검색옵션",("내용","작성자명","제목"))
+#         if cols[2].button("검색"):
+#                 if search_option == "내용":
+#                     result = get_by_comment(search_term)
+
+#                 elif search_option == "작성자명":
+#                     # result = get_by_author(search_term)
+                    
+#                     if search_term == "":
+#                         st.error(" '빈칸을 확인해주세요.' ")
+#                         result = pd.DataFrame(columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+#                     else:
+#                         result = get_by_author(search_term)
+#                 elif search_option == "제목":
+#                     if search_term == "":
+#                         st.error(" '빈칸을 확인해주세요.' ")
+#                         result = pd.DataFrame(columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+#                     else:
+#                         result = get_by_title(search_term)
+#                     # result=get_by_title(search_term)
+#                     # if search_term == "":
+#                     #     st.error("빈칸을 확인해주세요.")
+
+#                 s_result = pd.DataFrame(result, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+#                 st.dataframe(s_result, use_container_width=True)
+
+#                 # cur.execute("select date from suggestion11")
+#                 # a = cur.fetchall()
+#                 # st.write(a)
+
+
+
+#     # 목록
+#     container = st.container()
+#     with container:
+#         list = sugg_list()
+#         # st.write(list)
+#         df = pd.DataFrame(list, columns=['작성자명', '제목', '내용', '작성시각', '상태'])
+#         # st.dataframe(df.style.set_properties(subset=['내용'], **{'width': '1000px'}))
+#         st.dataframe(df, use_container_width=True)
+#         # st.table(df)
+
+# ======================================================
 
 # conn = sqlite3.connect('suggestion1.db', check_same_thread=False)
 # cur = conn.cursor()
@@ -184,4 +361,8 @@ def run_suggestions():
 #         # st.write(list)
 #         df = pd.DataFrame(list, columns=['작성자명', '제목', '내용', '작성시각'])
 #         st.dataframe(df)
+
+
+
+
 
--- title.py
+++ title.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
+import math
 
 
 def run_title():
@@ -13,15 +14,35 @@ def run_title():
     st.markdown("""
     ## 👑실거래 현황
     - *현재까지의 서울시 집에 대한 실거래가 현황입니다!*
+    - 기간 : 2022.01.01~ 2023.01.30 (계약일 기준)
     """)
     data_copy = data.copy()
-    data_copy.columns = ['구코드', '구이름', '동코드', '동이름', '번지', '번지_', 
-    '층수', '계약일', '전세/월세', '임대면적', '보증금', '임대료', 
-    '건물이름', '건축년도', '건물타입']
-    # data.columns = ['구코드', '구이름', '동코드', '동이름', '번지', '번지_', 
+    data_copy['FLR_NO'] = data_copy['FLR_NO'].astype(str) + '층'
+    cols = ['BOBN', 'BUBN']
+    data_copy['번지'] = data_copy[cols].apply(lambda row: '-'.join(row.values.astype(str))
+                                            if row['BUBN'] != 0
+                                            else row['BOBN'], axis=1)
+    data_copy['BLDG_NM'] = data_copy['BLDG_NM'].str.replace('아파트', '')
+    data_copy['BLDG_NM'] = data_copy['BLDG_NM'].str.replace('오피스텔', '')                             
+    cols1 = ['SGG_NM', 'BJDONG_NM', '번지', 'BLDG_NM', 'HOUSE_GBN_NM', 'FLR_NO']
+    data_copy['주소'] = data_copy[cols1].apply(lambda row:' '.join(row.values.astype(str)),axis=1)
+    data_copy = data_copy.drop(['SGG_CD', 'BJDONG_CD', 'SGG_NM', 'BJDONG_NM', 'BOBN', 'BUBN', 'FLR_NO', 'BLDG_NM', '번지', 'HOUSE_GBN_NM'], axis=1)
+    data_copy['RENT_AREA'] = data_copy['RENT_AREA'].apply(lambda x: math.trunc(x / 3.3058))
+    data_copy.columns = ['계약일', '전월세 구분', '임대면적(평)', '보증금(만원)', '임대료(만원)', '건축년도', '주소']
+    data_copy = data_copy[['계약일', '주소', '보증금(만원)', '임대료(만원)', '임대면적(평)', '건축년도', '전월세 구분']]
+    data_copy = data_copy.reset_index(drop=True)
+    data_copy.index = data_copy.index+1
+    st.write(data_copy)
+
+    # data_copy = data.copy()
+    # data_copy.columns = ['구코드', '구이름', '동코드', '동이름', '번지', '번지_', 
     # '층수', '계약일', '전세/월세', '임대면적', '보증금', '임대료', 
     # '건물이름', '건축년도', '건물타입']
-    st.write(data_copy)
+    # # data.columns = ['구코드', '구이름', '동코드', '동이름', '번지', '번지_', 
+    # # '층수', '계약일', '전세/월세', '임대면적', '보증금', '임대료', 
+    # # '건물이름', '건축년도', '건물타입']
+    # st.write(data_copy)
+    
 
     col1, col2 = st.columns(2)
     with col1:
@@ -29,24 +50,33 @@ def run_title():
         💵월세 실거래수 지역 순위
         - *현재 월세 실거래수 TOP 10*🥇
         """)
-        data_m = data[data['RENT_GBN']=='월세']
+        # 월세인 데이터 추출
+        data_m = data[data['RENT_GBN'] == '월세']
+        # 구, 동 합치기
         cols = ['SGG_NM', 'BJDONG_NM']
         data_m['주소'] = data_m[cols].apply(lambda row:' '.join(row.values.astype(str)),axis=1)
         data_addr = data_m['주소'].value_counts().rename_axis('주소').reset_index(name='거래 수')
+        #인덱스 재지정
         data_addr = data_addr.reset_index(drop=True)
         data_addr.index = data_addr.index+1
+        # st.write()보여주기
         st.write(data_addr.head(10))
     with col2:
         st.subheader("""
         💳전세 실거래수 지역 순위
         - *현재 전세 실거래수 TOP10*🏆
         """)
+        # 전세인 데이터 추출
         data_m = data[data['RENT_GBN']=='전세']
+        # 구, 동 합치기
         cols = ['SGG_NM', 'BJDONG_NM']
         data_m['주소'] = data_m[cols].apply(lambda row:' '.join(row.values.astype(str)),axis=1)
         data_addr = data_m['주소'].value_counts().rename_axis('주소').reset_index(name='거래 수')
+        # 인덱스 재지정
         data_addr = data_addr.reset_index(drop=True)
         data_addr.index = data_addr.index+1
+        
+        # st.write()보여주기
         st.write(data_addr.head(10))
 
     # st.title('TITLE')
